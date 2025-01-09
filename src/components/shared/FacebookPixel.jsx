345c9a4353ed7221cbe6273e3baa4efaeb9821b8
ashik2765 <ashikurrahman2765@gmail.com>
2025-01-09 21:32:36 +0600

    added facebook pixel

--- src/components/shared/FacebookPixel.jsx
+++ src/components/shared/FacebookPixel.jsx
@@ -5,29 +5,20 @@ import { useEffect } from "react";
 const FB_PIXEL_ID = "565553649536923"; // Your Pixel ID
 
 const initFacebookPixel = () => {
-  if (typeof window !== "undefined" && !window.fbq) {
-    (function (f, b, e, v, n, t, s) {
-      if (f.fbq) return;
-      n = f.fbq = function () {
-        n.callMethod
-          ? n.callMethod.apply(n, arguments)
-          : n.queue.push(arguments);
-      };
-      if (!f._fbq) f._fbq = n;
-      n.push = n;
-      n.loaded = !0;
-      n.version = "2.0";
-      n.queue = [];
-      t = b.createElement(e);
-      t.async = !0;
-      t.src = v;
-      s = b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t, s);
-    })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
+  !function (f, b, e, v, n, t, s) {
+    if (f.fbq) return; n = f.fbq = function () {
+      n.callMethod ?
+      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+    n.queue = []; t = b.createElement(e); t.async = !0;
+    t.src = v; s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+  }(window, document, 'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '2259293477774417');
+  fbq('track', 'PageView');
 
-    window.fbq("init", FB_PIXEL_ID);
-    window.fbq("track", "PageView");
-  }
 };
 
 export default function FacebookPixel() {
@@ -36,14 +27,9 @@ export default function FacebookPixel() {
   }, []);
 
   return (
-    <noscript>
-      <img
-        height="1"
-        width="1"
-        style={{ display: "none" }}
-        src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-        alt="facebook-pixel"
-      />
-    </noscript>
+    <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=2259293477774417&ev=PageView&noscript=1"
+    /></noscript>
+
   );
 }
